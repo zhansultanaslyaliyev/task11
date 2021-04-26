5b1b65ac4391429069d620a3e126e69675ff64b9
Zhansultan <zhansultan.aslyaliyev@gmail.com>
2021-04-26 14:03:38 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/task11/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.task11
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Task 11',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> {
+  int _rating = 0;
+  @override
+  Widget build(BuildContext context) {
+    double _size = 50;
+
+    return Scaffold(
+      appBar: AppBar(
+      title: Text('Task 11'),
+      ),
+    body: Center(
+    child: Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Container(
+      child: IconButton(
+      icon: (_rating >= 2
+    ? Icon(
+    Icons.star,
+    size: _size,
+    )
+        : Icon(
+        Icons.star_border,
+      size: _size,
+    )),
+    color: Colors.indigo[500],
+    iconSize: _size,
+    onPressed: () {
+        setState(() {
+          _rating = 1;
+        });
+        }
+      ),
+      ),
+
+    Container(
+    child: IconButton(
+    icon: (_rating >= 2
+    ? Icon(
+    Icons.star,
+    size: _size,
+    )
+        : Icon(
+    Icons.star_border,
+    size: _size,
+    )),
+    color: Colors.indigo[500],
+    iconSize: _size,
+    onPressed: () {
+    setState(() {
+    _rating = 2;
+    });
+    },
+    ),
+    ),
+
+    Container(
+    child: IconButton(
+    icon: (_rating >= 3
+    ? Icon(
+    Icons.star,
+    size: _size,
+    )
+        : Icon(
+    Icons.star_border,
+    size: _size,
+    )),
+    color: Colors.yellow,
+    iconSize: _size,
+    onPressed: () {
+    setState(() {
+    _rating = 3;
+    });
+    },
+    ),
+    ),
+    ],
+    ),
+    ),
+    );
+  }
+}
